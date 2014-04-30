adcd377c105266da09e49eda3fbedc2da1b42841
Maksim Pecherskiy <max@maksimize.com>
2014-04-29 17:28:00 -0700

    Testing for votes.

--- test/app.js
+++ test/app.js
@@ -1,12 +1,16 @@
 var request = require('supertest');
 var app = require('../app.js');
+var webhookMocks = require('../helpers/webhookMocks');
 
 describe('POST /votes/sms', function() {
   it('should return 200 OK', function(done) {
-    var bodyString = "Body=Gh&MessageSid=SMb251b49a699fa9de11360ab633e48632&FromZip=60712&From=%2B17736777755&FromCountry=US&SmsStatus=received&FromCity=CHICAGO&ApiVersion=2010-04-01&To=%2B19164263342&NumMedia=0&ToZip=&ToCountry=US&ToState=CA&AccountSid=ACe0c25418c2742c5b49f8f8041899b360&SmsSid=SMb251b49a699fa9de11360ab633e48632&ToCity=&FromState=IL&SmsMessageSid=SMb251b49a699fa9de11360ab633e48632";
-
+    var smsWebhook = webhookMocks.twilioSMS;
     request(app)
-      .post('/votes/sms', {body: bodyString})
+      .post('/votes/sms')
+      .send(smsWebhook.data)
+      .set('Content-Type', smsWebhook.headers.ContentType)
+      .set('User-Agent', smsWebhook.headers.UserAgent)
+      .set('X-Twilio-Signature', smsWebhook.headers.XTwilioSignature)
       .expect(200, done);
   });
 });
